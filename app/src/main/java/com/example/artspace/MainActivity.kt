d0b5c0ae0324a0c37fcdbf1df794c604e4b8120a
Semkov Stanislav <34599107+Sem1judo@users.noreply.github.com>
2022-11-13 00:00:35 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -42,72 +42,64 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 @Preview
 fun ArtSpaceApp() {
     var userChoose by remember {
         mutableStateOf(1)
     }
-    println("userChoose = $userChoose")
 
     when (userChoose) {
         0 -> ArtSpaceDisplay(
-            image = painterResource(id = R.drawable.done),
+            image = painterResource(id = R.drawable.one),
             title = stringResource(id = R.string.title),
             fullName = stringResource(id = R.string.fullName),
-            year = stringResource(id = R.string.year)
+            year = stringResource(id = R.string.year),
+            { userChoose++ },
+            { userChoose-- }, userChoose
+
         )
         1 -> ArtSpaceDisplay(
-            image = painterResource(id = R.drawable.android_logo),
+            image = painterResource(id = R.drawable.two),
             title = stringResource(id = R.string.title),
             fullName = stringResource(id = R.string.title),
-            year = stringResource(id = R.string.title)
+            year = stringResource(id = R.string.title),
+            { userChoose++ },
+            { userChoose-- }, userChoose
         )
         2 -> ArtSpaceDisplay(
-            image = painterResource(id = R.drawable.androidparty),
+            image = painterResource(id = R.drawable.three),
             title = stringResource(id = R.string.fullName),
             fullName = stringResource(id = R.string.fullName),
-            year = stringResource(id = R.string.fullName)
+            year = stringResource(id = R.string.fullName),
+            { userChoose++ },
+            { userChoose-- }, userChoose
+        )
+        3 -> ArtSpaceDisplay(
+            image = painterResource(id = R.drawable.four),
+            title = stringResource(id = R.string.fullName),
+            fullName = stringResource(id = R.string.fullName),
+            year = stringResource(id = R.string.fullName),
+            { userChoose++ },
+            { userChoose-- }, userChoose
         )
     }
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
 
-        Row(horizontalArrangement = Arrangement.Center) {
-            if (userChoose > 0)
-                Button(
-                    onClick = { userChoose-- },
-                    Modifier
-                        .padding(15.dp)
-                        .weight(1f)
-
-                ) {
-                    Text(text = stringResource(R.string.previous))
-
-                }
-            if (userChoose < 2) {
-                Button(
-                    onClick = { userChoose++ },
-                    Modifier
-                        .padding(15.dp)
-                        .weight(1f)
-                ) {
-                    Text(text = stringResource(R.string.next))
-                }
-            }
-        }
-    }
 }
 
 
 @Composable
-fun ArtSpaceDisplay(image: Painter, title: String, fullName: String, year: String) {
+fun ArtSpaceDisplay(
+    image: Painter, title: String, fullName: String, year: String, onClickNext: () -> Unit,
+    onClickPrev: () -> Unit, cur: Int
+) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.background(Color.White)
+        modifier = Modifier
+            .background(Color.White)
+            .border(2.dp, Color.LightGray),
+        verticalArrangement = Arrangement.Top
     ) {
         ArtworkWall(image = image)
         ArtworkDescription(
@@ -115,47 +107,105 @@ fun ArtSpaceDisplay(image: Painter, title: String, fullName: String, year: Strin
             fullName = fullName,
             year = year
         )
+        ArtworkNavigation(onClickNext, onClickPrev, cur)
     }
 
 }
 
 @Composable
 fun ArtworkWall(image: Painter) {
-    Image(
-        painter = image,
-        contentDescription = image.toString(),
-        contentScale = ContentScale.Fit,
+    Card(
+        elevation = 4.dp,
         modifier = Modifier
-            .padding(15.dp)
-            .border(2.dp, color = Color.Black)
+            .padding(20.dp, 5.dp)
+            .border(2.dp, Color.Gray),
     )
-}
-
-@Composable
-fun ArtworkDescription(title: String, fullName: String, year: String) {
-    Column(
-        modifier = Modifier.border(2.dp, color = Color.Black),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-    ) {
-        Text(
-            text = title, fontSize = 36.sp,
-            fontWeight = FontWeight.Bold
+    {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .background(Color.White),
+            verticalArrangement = Arrangement.Top
         )
-        Row(horizontalArrangement = Arrangement.Center) {
-            Text(
-                text = fullName,
-                modifier = Modifier.padding(5.dp),
-                fontSize = 24.sp,
-            )
-            Text(
-                text = "($year)",
-                modifier = Modifier.padding(5.dp),
-                fontSize = 24.sp,
+        {
+            Image(
+                painter = image,
+                contentDescription = image.toString(),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .padding(30.dp)
+
             )
         }
     }
 }
 
+@Composable
+fun ArtworkDescription(
+    title: String,
+    fullName: String,
+    year: String
+) {
+    Card(
+        elevation = 4.dp,
+        modifier = Modifier.padding(20.dp, 10.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(1f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+        ) {
+            Text(
+                text = title, fontSize = 32.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Row(horizontalArrangement = Arrangement.Center) {
+                Text(
+                    text = fullName,
+                    modifier = Modifier.padding(5.dp),
+                    fontSize = 24.sp,
+                )
+                Text(
+                    text = "($year)",
+                    modifier = Modifier.padding(5.dp),
+                    fontSize = 24.sp,
+                )
+            }
+        }
+    }
 
+}
 
+@Composable
+fun ArtworkNavigation(
+    onClickNext: () -> Unit,
+    onClickPrev: () -> Unit,
+    cur: Int
+) {
+
+    Row(
+        horizontalArrangement = Arrangement.Center
+    ) {
+        if (cur > 0) {
+            Button(
+                onClick = onClickPrev,
+                Modifier
+                    .padding(20.dp, 0.dp)
+                    .weight(1f)
+            ) {
+                Text(text = stringResource(R.string.previous))
+            }
+        }
+        if (cur < 3) {
+            Button(
+                onClick = onClickNext,
+                Modifier
+                    .padding(20.dp, 0.dp)
+                    .weight(1f)
+            ) {
+                Text(text = stringResource(R.string.next))
+            }
+        }
+    }
+}
